409df6ed3ccb1eef5623626eccedffcb512cbf84
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2020-01-05 16:38:17 -0800

    project setup

---
+++ foodie-fun/src/reducers/userReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  isFetching: false,
+  user: {}
+}
+
+function userReducer(state = initialState, action) {
+  switch(action.type) {
+
+    default:
+      return state
+  }
+}
+
+export default userReducer;
